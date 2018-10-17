30f1f882e84e6e086c9b573e433aa9112873167b
Alma Maggey <almamaggey@gmail.com>
2018-10-17 09:31:31 +0000

    up

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,9 +1,11 @@
 function setup(){
-  createCanvas(300,300);
-   background(255,0,0);
-   fill(150);
-   ellipse(100,100,50,50);
+  createCanvas(630,750);
+   background(260,0,0);
+   fill(200);
+   ellipse(310,100,100,50);
    fill(0);
-   textSize(20);
+   textSize(40);
    text("Halló heimur",200,40);
+   rect(440,10,30,30)
+   ellipse(170,25,30,30);
 }// Hér kemur kóðinn þinn:
