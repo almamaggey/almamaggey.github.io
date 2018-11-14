cba2cbf0dd9a9cdc7166be251b23325e866bb04e
Alma Maggey <almamaggey@gmail.com>
2018-11-14 08:50:05 +0000

    up

---
+++ P6/sketch.js
@@ -0,0 +1,43 @@
+var x = 100;
+var y = 100;
+var hradiX = 5;
+var hradiY = 11;
+var boltiStaerd = 20;
+var stig = 0;
+var spadiBreidd = 150;
+var spadiThykkt = 25;
+var spadiY;
+var tré//https://www.pexels.com/search/christmas/
+
+function preload() {
+  tré = loadImage('myndskopp.jpg');
+}
+function setup() {
+	createCanvas(600,500);
+	spadiY = height - 45;
+	rectMode(CENTER);
+	textFont("Courier New",16);
+	textStyle(BOLD);
+	background(tré);
+}
+
+function draw() {
+  background(179, 233, 245);
+  x = x + hradiX;
+  y = y + hradiY;
+  if ((x > width-boltiStaerd/2) || (x < boltiStaerd/2))  {
+    hradiX = hradiX * -1;
+  }
+  if ((y > height-boltiStaerd/2) || (y < boltiStaerd)) {
+    hradiY = hradiY * -1;
+  }
+	if(abs(spadiY - y) < boltiStaerd/2 + spadiThykkt/2 && abs(mouseX - x) < boltiStaerd/2 + spadiBreidd/2){
+    hradiY = hradiY * -1;
+		stig = stig + 1;
+	}
+  fill(245, 179, 241);
+  ellipse(x, y, boltiStaerd, boltiStaerd);
+	rect(mouseX,spadiY ,spadiBreidd,spadiThykkt);
+	fill(0);
+	text("Stig: " + stig,20,20);
+}
