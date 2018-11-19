be6fe8538ece0ffcbfa8f081676d186e87b2010d
Alma Maggey <almamaggey@gmail.com>
2018-11-19 12:09:12 +0000

    u

--- P7/sketch.js
+++ P7/sketch.js
@@ -1,45 +1,31 @@
-var x = 100;
-var y = 100;
-var hradiX = 5;
-var hradiY = 11;
-var boltiStaerd = 20;
-var stig = 0;
-var spadiBreidd = 150;
-var spadiThykkt = 25;
-var spadiY;
-var tré//https://www.pexels.com/search/christmas/
-
-function preload() {
-  tré = loadImage('myndskopp.jpg');
-}
 function setup() {
-	createCanvas(600,500);
-	spadiY = height - 45;
-	rectMode(CENTER);
-	textFont("Courier New",16);
-	textStyle(BOLD);
-	background(tré);
-	colorMode(HSB);
-	frameRate(50);
+  createCanvas(700,400);
 }
 
 function draw() {
-	background(tré);
-  x = x + hradiX;
-  y = y + hradiY;
-  if ((x > width-boltiStaerd/2) || (x < boltiStaerd/2))  {
-    hradiX = hradiX * -1;
-  }
-  if ((y > height-boltiStaerd/2) || (y < boltiStaerd)) {
-    hradiY = hradiY * -1;
-  }
-	if(abs(spadiY - y) < boltiStaerd/2 + spadiThykkt/2 && abs(mouseX - x) < boltiStaerd/2 + spadiBreidd/2){
-    hradiY = hradiY * -1;
-		stig = stig + 1;
-	}
-  fill(245, 179, 241);
-  ellipse(x, y, boltiStaerd, boltiStaerd);
-	rect(mouseX,spadiY ,spadiBreidd,spadiThykkt);
-	fill(255, 255, 255);
-	text("Stig: " + stig,20,20);
+  background(255, 147, 247);
+  blúbb(mouseX, mouseY,10,60,100);
+  blúbb(100,100,20,140,60);
+  blúbb(300,200,30,100.50);
+  // x var 200 og y var 150
+}
+
+// Teiknar geimveruna zoog
+function blúbb(x,y, augun, búkur, haus){
+  rectMode(CENTER);
+  // Teiknum búkinn
+  fill(6, 249,30);
+  rect(x,y+50,40,búkur);
+  // Teiknum hausinn
+  fill(255);
+  ellipse(x,y, 110, haus);
+  // Teiknum augun
+  fill(100);
+  ellipse (x-25,y, augun,40);
+  ellipse (x+25,y, 20,augun);
+  // Teiknum fæturna
+  line (x-20, y+100, x-20, y+150);
+  line (x+20, y+100, x+20, y+150);
+  line(x+20, y+50, x+50, y+80);
+  line(x-50,y+30 ,x-20,y+50);
 }
